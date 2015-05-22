678330a141ee6d80f1b97afce602afae957c6a5c
James Petry <gavilancomun@gmail.com>
2015-05-22 12:38:20 +0100

    draw

--- src/kakuro/core.fs
+++ src/kakuro/core.fs
@@ -19,25 +19,49 @@ type DownAcross(down : int, across : int) =
     interface IDraw with
         member __.draw() = sprintf "   %2d\\%2d  " down across
 
+type Value(values : Set<int>) = 
+    interface IDraw with
+        member __.draw() = 
+            if 1 = values.Count then 
+                values
+                |> Set.map (fun x -> "    " + x.ToString() + "    ")
+                |> String.concat ""
+            else 
+                " " + ([ 1..9 ]
+                       |> List.map (fun x -> 
+                              if Set.contains x values then x.ToString()
+                              else ".")
+                       |> String.concat "")
+
 let a (across : int) = Across(across)
 let d (down : int) = Down(down)
 let da (down : int, across : int) = DownAcross(down, across)
 let e() = Empty()
+let v = Value(set [ 1..9 ])
 
-let drawRow(row: List<IDraw>) = 
-  (row
-  |> List.map (fun x -> x.draw())
-  |> String.concat "") + "\n"
+let drawRow (row : List<IDraw>) = 
+    (row
+     |> List.map (fun x -> x.draw())
+     |> String.concat "")
+    + "\n"
 
-let drawGrid(grid: List<List<IDraw>>) =
-  grid
-  |> List.map (fun row -> drawRow(row))
-  |> String.concat ""
+let drawGrid (grid : List<List<IDraw>>) = 
+    grid
+    |> List.map (fun row -> drawRow (row))
+    |> String.concat ""
 
 [<EntryPoint>]
 let main argv = 
     let grid1 : List<List<IDraw>> = 
-        [[ e(); d 1; a 2; da(3, 4) ]
-         [ e(); d 1; a 2; da(3, 4) ]]
+        [ [ e()
+            d 1
+            a 2
+            da (3, 4)
+            v ]
+          [ v
+            e()
+            d 1
+            a 2
+            da (3, 4) ] ]
     printf "%s" <| drawGrid grid1
     0 // return an integer exit code
