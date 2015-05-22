69e608b385262db33ec9142c8d40c5528b572479
James Petry <gavilancomun@gmail.com>
2015-05-22 14:49:15 +0100

    solve

--- src/kakuro/core.fs
+++ src/kakuro/core.fs
@@ -8,18 +8,29 @@ type Empty() =
         member __.draw() = "   -----  "
 
 type Down(down : int) = 
+    
     interface IDraw with
         member __.draw() = sprintf "   %2d\\--  " down
+    
+    member __.down = down
 
 type Across(across : int) = 
+    
     interface IDraw with
         member __.draw() = sprintf "   --\\%2d  " across
+    
+    member __.across = across
 
 type DownAcross(down : int, across : int) = 
+    
     interface IDraw with
         member __.draw() = sprintf "   %2d\\%2d  " down across
+    
+    member __.down = down
+    member __.across = across
 
 type Value(values : Set<int>) = 
+    
     interface IDraw with
         member __.draw() = 
             if 1 = values.Count then 
@@ -32,6 +43,8 @@ type Value(values : Set<int>) =
                               if Set.contains x values then x.ToString()
                               else ".")
                        |> String.concat "")
+    
+    member __.values = values
 
 let a (across : int) = Across(across)
 let d (down : int) = Down(down)
@@ -49,6 +62,45 @@ let drawGrid (grid : List<List<IDraw>>) =
     grid
     |> List.map (fun row -> drawRow (row))
     |> String.concat ""
+
+let allDifferent (nums : List<int>) = nums.Length = (set nums).Count
+
+let rec permute (vs : List<Value>, target : int, soFar : List<int>) = 
+    if target >= 1 then 
+        if soFar.Length = (vs.Length - 1) then [ soFar @ [ target ] ]
+        else 
+            (List.nth vs soFar.Length).values
+            |> Seq.collect (fun v -> permute (vs, (target - v), (soFar @ [ v ])))
+            |> List.ofSeq
+    else []
+
+let permuteAll (vs : List<Value>, total : int) = permute (vs, total, [])
+let isPossible (cell : Value, n : int) = Set.contains n cell.values
+
+let rec transpose matrix = 
+    match matrix with // matrix is a list<list<int>>
+    | row :: rows -> // case when the list of rows is non-empty
+        match row with // rows is a list<int>
+        | col :: cols -> // case when the row is non-empty
+            // Take first elements from all rows of the matrix
+            let first = List.map List.head matrix
+            // Take remaining elements from all rows of the matrix
+            // and then transpose the resulting matrix
+            let rest = transpose (List.map List.tail matrix)
+            first :: rest
+        | _ -> []
+    | _ -> []
+
+let solveStep (cells : List<Value>, total : int) = 
+    let final = cells.Length - 1
+    
+    let perms = 
+        permuteAll (cells, total)
+        |> List.filter (fun p -> isPossible ((List.nth cells final), (List.nth p final)))
+        |> List.filter allDifferent
+    perms
+    |> transpose
+    |> List.map (fun p -> Value(set p))
 
 [<EntryPoint>]
 let main argv = 
